2b3fb381bd20f9b5c308c84424624961b1b45f02
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2024-11-11 13:46:27 +0000

    Connect to RDS DB by disabling SSL

--- src/lib/db/pool.js
+++ src/lib/db/pool.js
@@ -1,4 +1,5 @@
 import pkg from 'pg'; // Import the default export from the pg package
+import 'dotenv/config';
 
 // Destructure Pool from the default export
 const { Pool } = pkg;
@@ -9,7 +10,14 @@ const host = process.env.POSTGRES_HOST ? process.env.POSTGRES_HOST : "localhost"
 const database = process.env.POSTGRES_DATABASE ? process.env.POSTGRES_DATABASE : "time_tracker";
 const password = process.env.POSTGRES_PASSWORD ? process.env.POSTGRES_PASSWORD : "password";
 
-const creds = {user, host, database, password, port}
+const creds = {
+    user,
+    host,
+    database,
+    password,
+    port,
+    ssl: { rejectUnauthorized: false }  // Add this line to enable SSL and ignore certificate validation
+}
 
 // Create a pool instance with your PostgreSQL connection details
 const pool = new Pool(creds);
