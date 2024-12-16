59a6c73ed83a67d268215f0eb1f569543d740da3
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2024-12-16 12:57:49 +0000

    Revert accidental +page.server.js deletion

---
+++ src/routes/+page.server.js
@@ -0,0 +1,45 @@
+/**
+ * @type {import('./$types').PageServerLoad}
+ * The server always loads task data from DB on initial page hydration
+ * and automatically passes it back to the client in +page.svelte.
+ */
+export async function load({ fetch }) {
+	/**
+	 * @type {import("$lib/stores").Task[]}
+	 */
+	let tasks = [];
+	let error = '';
+
+	try {
+		console.log('Client requesting first page load!');
+		console.log('Fetching tasks from backend.');
+		const res = await fetch('/api/tasks');
+		console.log('Fetched tasks from DB.');
+		if (res.ok) {
+			tasks = await res.json();
+			if (tasks.length == 0) {
+				console.log('No tasks found.');
+				const task = await createTask(fetch);
+				tasks.push(task);
+			}
+		} else {
+			error = 'Server failed to fetch tasks from DB';
+			console.error(error);
+		}
+	} catch (err) {
+		console.error(err);
+		error = 'An error occurred while fetching tasks';
+	}
+
+	return { tasks, error };
+}
+
+/**
+ * @param {typeof fetch} fetch
+ * @returns {Promise<import("$lib/stores").Task>}
+ */
+async function createTask(fetch) {
+	const res = await fetch('/api/tasks', { method: 'POST' });
+	const { task } = await res.json();
+	return task;
+}
