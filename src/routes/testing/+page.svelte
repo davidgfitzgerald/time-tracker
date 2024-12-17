53cb24541738885b94f1f531ade7d67391f7bb85
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2024-12-17 21:23:24 +0000

    Add dummy testing page for experiments

---
+++ src/routes/testing/+page.svelte
@@ -0,0 +1,34 @@
+<h1>Testing</h1>
+
+<div class="container">
+
+    <div class="centered">
+        Text One
+    </div>
+    <div class="centered">
+        Text Two
+    </div>
+</div>
+
+<style>
+    .container {
+        display: flex;
+        gap: 1rem;
+    }
+    .centered {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 200px;
+        width: 200px;
+        border: 1px solid black;
+    }
+
+    .container:hover .centered {
+        cursor: pointer;
+        transition: all 0.1s ease;
+        border: 15px solid lightgrey;
+        box-sizing: border-box;
+    }
+
+</style>
